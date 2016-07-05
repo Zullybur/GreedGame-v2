bc825c631d3c8920d0c0306a0a2d8f346bc7d7e5
Matthew Casiro <mattcasiro@gmail.com>
2016-07-05 11:29:24 -0700

    A player can be created with a given name, and has a score and number property.

---
+++ lib/greed/player.rb
@@ -0,0 +1,34 @@
+module Greed
+  class Player
+    include Comparable
+
+    attr_accessor :score
+    attr_reader :number, :name
+
+    @@count = 0
+
+    def self.count
+      @@count
+    end
+
+    def initialize(name)
+      @name = name
+      @score = 0
+      set_number
+    end
+
+    def <=>(other)
+      score <=> other.score
+    end
+
+    def inspect
+      return "P#{@number}(#{@name}): #{@score}"
+    end
+
+    private
+    def set_number
+      @@count = @@count + 1
+      @number = @@count
+    end
+  end
+end
