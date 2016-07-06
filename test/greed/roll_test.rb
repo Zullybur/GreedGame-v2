122c48a991946e84dc19b363ff8467c83294c601
Matthew Casiro <mattcasiro@gmail.com>
2016-07-06 11:40:33 -0700

    Tests should not check any scoring functionality

--- test/greed/roll_test.rb
+++ test/greed/roll_test.rb
@@ -12,16 +12,33 @@ module Greed
 
     def test_roll_has_attributes
       roll = Roll.new(Array.new(5) { Die.new })
-      assert_not_nil roll.score
-      assert_not_nil roll.live_dice
+      assert_not_nil roll.dice
+      assert roll.dice.size > 0
     end
 
-    def test_roll_calculates_score
+    def test_subsequent_rolls_change_dice_values
+      dice = Array.new(5) { Die.new }
+      roll1 = Roll.new(dice).dice.map { |x| x.value}
+      roll2 = Roll.new(dice).dice.map { |x| x.value}
+      roll3 = Roll.new(dice).dice.map { |x| x.value}
+      # Allow for the rare possibility of randomly rolling the
+      # same values twice, by checking three arrays against each other
+      assert !roll1.eql?(roll2) || !roll2.eql?(roll3) || !roll1.eql?(roll3)
+    end
+
+    def test_to_hash_returns_hash
       roll = Roll.new(Array.new(5) { Die.new })
-      puts "Manual check (rolled dice):"
-      roll.dice.each { |die| puts die.value }
-      puts "Manual check (live dice):"
-      roll.live_dice.each { |die| puts die.value }
+      assert_equal Hash, roll.to_hash.class
+    end
+
+    def test_to_hash_returns_arrays_of_dice_as_values
+      roll = Roll.new(Array.new(5) { Die.new })
+      roll.to_hash.each do |key, array_of_dice|
+        assert_equal Array, array_of_dice.class
+        array_of_dice.each do |die|
+          assert_equal Die, die.class
+        end
+      end
     end
   end
 end
