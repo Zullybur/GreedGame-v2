817f0e76e6b9535452426f2da5ac845137967169
Matthew Casiro <mattcasiro@gmail.com>
2016-07-07 11:41:17 -0700

    Refactor based on PR feedback
    - attr_readers on one line
    - rename roll variable in roll method to avoid duplicate names
    - Turn handles re-setting live dice if none remain (update_live_dice)
    - spacing correction on dupdate_total_score

--- lib/greed/turn.rb
+++ lib/greed/turn.rb
@@ -1,11 +1,9 @@
 require 'greed/roll'
+require 'greed/die'
 
 module Greed
   class Turn
-    attr_reader :dice
-    attr_reader :live_dice
-    attr_reader :total_score
-    attr_reader :roll_score
+    attr_reader :dice, :live_dice, :total_score, :roll_score
 
     def initialize
       @live_dice = Array.new(5) { Die.new }
@@ -13,20 +11,30 @@ module Greed
     end
 
     def roll
-      roll = Roll.new(@live_dice)
-      @dice = roll.dice
-      @roll_score = roll.score
-      @live_dice = roll.live_dice
+      @roll_result = Roll.new(@live_dice)
+      @dice = @roll_result.dice
+      @roll_score = @roll_result.score
+      update_live_dice
       update_total_score
     end
 
     private
+    # Get live dice from roll, if no live dice remain then
+    # create five new dice for the player to roll.
+    def update_live_dice
+      @live_dice = if @roll_result.live_dice.size > 0
+                     @roll_result.live_dice
+                   else
+                     Array.new(5) { Die.new }
+                   end
+    end
+
     def update_total_score
       @total_score = if @roll_score > 0
-        @total_score + @roll_score
-      else
-        0
-      end
+                       @total_score + @roll_score
+                     else
+                       0
+                     end
     end
   end
 end
