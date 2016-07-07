4b4d52c0fa1e6b602a9232a440f5f5019a356d52
Matthew Casiro <mattcasiro@gmail.com>
2016-07-07 11:43:58 -0700

    Test turn.rb

--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,4 @@
+printf "~~Die Tests~~\n"
 ruby -I . -I lib test/greed/die_test.rb
 printf "\n~~Player Tests~~\n"
 ruby -I . -I lib test/greed/player_test.rb
@@ -5,5 +6,5 @@ printf "\n~~Roll Tests~~\n"
 ruby -I . -I lib test/greed/roll_test.rb
 printf "\n~~RollScore Tests~~\n"
 ruby -I . -I lib test/greed/roll_score_test.rb
-#printf "\n~~Turn Tests~~\n"
-#ruby -I . -I lib test/greed/turn_test.rb
+printf "\n~~Turn Tests~~\n"
+ruby -I . -I lib test/greed/turn_test.rb
