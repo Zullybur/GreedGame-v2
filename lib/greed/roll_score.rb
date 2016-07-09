9e700c85af91616c5dcbc1ddd76631f8c6403d4c
Matthew Casiro <mattcasiro@gmail.com>
2016-07-08 17:06:15 -0700

    Restructured to provide value and non scoring dice JIT

--- lib/greed/roll_score.rb
+++ lib/greed/roll_score.rb
@@ -1,48 +1,48 @@
 module Greed
   class RollScore
-    attr_reader :value, :non_scoring_dice
 
     def initialize(dice)
-      raise ArgumentError, "Valid roll object not provided" unless dice
+      raise ArgumentError, "Valid dice array not provided" unless dice
       @dice = dice
-      update_score_value
-      update_non_scoring_dice
     end
 
+    def non_scoring_dice
+      array = []
+      dice_to_hash.each do |face, dice|
+        num_non_scoring(face, dice).times { array << dice.pop.dup }
+      end
+      array
+    end
+
+    def value
+      dice_to_hash.inject(0) do |score, (face, dice)|
+        # Score is points-from-triples + points-from-individuals
+        score + triples_score(face, dice.count) + individual_die_score(face, dice.count)
+      end
+    end
 
     private
-    def update_non_scoring_dice
-      dice_array = []
-      dice_to_hash.each do |face, dice|
-        # live dice are dice that do not contribute to the score
-        if face != 1 && face != 5
-          (dice.size % 3).times do
-            dice_array << dice.pop
-          end
-        end
-      end
-      @non_scoring_dice = dice_array
+    ScoringSet = 3
+    attr_reader :dice
+
+    # non scoring dice exclude any face value that returns an individual
+    # score, and sets of three dice otherwise
+    def num_non_scoring(face, dice)
+      face == 1 || face == 5 ? 0 : dice.size % ScoringSet
     end
 
     # Convert a dice collection to a hash containing arrays
     # of dice collected by face value
     def dice_to_hash
       hash = Hash.new { |h,k| h[k] = Array.new }
-      @dice.each { |die| hash[die.value] << die }
+      dice.each { |die| hash[die.value] << die }
       hash
-    end
-
-    def update_score_value
-      @value = dice_to_hash.inject(0) do |score, (face, dice)|
-                 # Score is points-from-triples + points-from-individuals
-                 score + triples_score(face, dice.size) + individual_die_score(face, dice.size)
-               end
     end
 
     # 100 points for each 1 (not in a set of three)
     # 50 points for each 5 (not in a set of three)
     def individual_die_score(face, count)
-      count %= 3
+      count %= ScoringSet
       case face
       when 1 then count * 100
       when 5 then count * 50
@@ -54,7 +54,7 @@ module Greed
     def triples_score(face, count)
       # Three 1's is worth 1000, not 100
       face = 10 if face == 1
-      triple_score = (count / 3) * (100 * face)
+      triple_score = (count / ScoringSet) * (100 * face)
     end
   end
 end
