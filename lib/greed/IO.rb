910fb568a7f29fe024e6d6cd2e32e378b7560761
Matthew Casiro <mattcasiro@gmail.com>
2016-07-12 16:15:58 -0700

    IO returns input instead of state adjustments

--- lib/greed/IO.rb
+++ lib/greed/IO.rb
@@ -5,17 +5,18 @@ module Greed
     class << self
       DecorativeLine =  "~~~~~~~~~~~~~~~~~"
 
+      #unused
       def display_main_menu
         loop do
           system('clear')
           puts "Welcome to Greed!\n#{DecorativeLine}"
           puts "(R)ules ~~ (P)lay"
           input = get_char_input.upcase
-          break "rules" if input == "R"
-          break "init game" if input == "P"
+          break input if input == "R" || input == "P"
         end
       end
 
+      #unused
       def display_rules
         system('clear')
         system('cat GREED_RULES.txt | more')
@@ -25,12 +26,14 @@ module Greed
         end
       end
 
+      #unused
       def display_get_num_players
         system('clear')
         puts "Please enter the number of players (between 1 and 9):"
         get_int_input(1,9)
       end
 
+      #unused
       def display_get_players(num)
         players = []
         (1..num).each do |player_num|
@@ -43,35 +46,36 @@ module Greed
 
       def display_game_state(game)
         system('clear')
-        puts "Current Player: #{game.current_player.name} (#{game.current_player.score})\n#{DecorativeLine}"
-        puts "Scores (next player at top)"
-        game.active_players.reverse.each { |player| puts "#{player.name}: #{player.score}" }
+        puts "Players (in turn order)\n#{DecorativeLine}"
+        game.players.each { |player| puts "#{player.name}: #{player.score}" }
+        puts "The game is #{game.final_round? ? "" : "**not**"} in the final round"
         puts "#{DecorativeLine}\nPress ENTER to continue"
         gets
       end
 
-      def display_get_roll_decision(game)
+      def display_reroll?(game, turn, roll)
         loop do
           system('clear')
           puts "Current Player: #{game.current_player.name}"
           puts "Current Score: #{game.current_player.score}"
-          puts "Last Roll: #{game.turn.dice.map { |die| die.value } }"
-          puts "Roll Score: #{game.roll.score}"
-          puts "Accumulated Score: #{game.turn.total_score}"
+          puts "Last Roll: #{turn.dice.map { |die| die.value } }"
+          puts "Roll Score: #{roll.score}"
+          puts "Accumulated Score: #{turn.total_score}"
           puts DecorativeLine
-          puts "You have #{game.turn.live_dice.size} live dice. Would you like to roll again?"
+          puts "You have #{turn.live_dice.size} live dice. Would you like to roll again?"
           puts DecorativeLine
           puts "(R)oll ~~ (E)nd Turn ~~ (G)ame State"
           input = get_char_input.upcase
-          break input if input == "R" || input == "E"
+          break true if input == "R"
+          break false if input == "E"
           display_game_state(game) if input == "G"
         end
       end
 
-      def display_player_busted(game)
+      def display_player_busted(player, dice)
         system('clear')
-        puts "#{game.current_player.name}, you busted!!!"
-        puts "Roll: #{game.turn.dice.map { |die| die.value } }\n#{DecorativeLine}"
+        puts "#{player.name}, you busted!!!"
+        puts "Roll: #{dice.map { |die| die.value } }\n#{DecorativeLine}"
         puts "Press ENTER to switch to the next player"
         gets
       end
@@ -79,23 +83,16 @@ module Greed
       def display_final_round_warning(game)
         system('clear')
         puts "WARNING: #{game.current_player.name}, this is your last turn!"
-        puts "The current high score is: #{game.final_player_states.sort.reverse[0].score}"
+        puts "The current high score is: #{game.players.sort.reverse[0].score}"
         puts "#{DecorativeLine}\nPress ENTER to conitnue"
         gets
       end
 
-      def display_game_results(final_state)
-        loop do 
-          system('clear')
-          puts "Final Scores\n#{DecorativeLine}"
-          final_state.sort.reverse.each { |player| puts "#{player.name}: #{player.score}" }
-          puts DecorativeLine
-          puts "Would you like to play again?"
-          puts "(Y)es ~~ (N)o"
-          input = get_char_input.upcase
-          break "main menu" if input == "Y"
-          break "exit" if input == "N"
-        end
+      def display_game_results(players)
+        system('clear')
+        puts "Final Scores\n#{DecorativeLine}"
+        players.sort.reverse.each { |p| puts "#{p.name}: #{p.score}" }
+        puts DecorativeLine
       end
 
       def get_char_input
